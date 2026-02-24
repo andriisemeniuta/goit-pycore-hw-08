76c7d6f5171f289605088380413044867e8ef18a
Andrej <pytpoint@gmail.com>
2026-02-24 14:54:29 +0100

    com1

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-print(11111)
-print(11111)
-print(11111)
+class Iterable:
+    MAX_VALUE = 10
+
+    def __init__(self):
+        self.current_value = 0
+
+    def __next__(self):
+        if self.current_value < Iterable.MAX_VALUE:
+            self.current_value += 1
+            return self.current_value
+        raise StopIteration
+
+
+class CustomIterator:
+    def __iter__(self):
+        return Iterable()
+
+
+
+
+c = CustomIterator()  # створено об'єкт, що ітерується.
+for i in c:  # зустрівся ітераційний контекст (цикл for) та об'єкт, що ітерується, в ньому, екземпляр класу c
+    print(i)
---
+++ test.py
@@ -0,0 +1,16 @@
+class SimpleCounter:
+    def __init__(self):
+        self.current = 0   # состояние
+
+    def __iter__(self):
+        return self        # говорим for: "я сам итератор"
+
+    def __next__(self):
+        if self.current < 3:
+            self.current += 1
+            return self.current
+        else:
+            raise StopIteration
+        
+for number in SimpleCounter():
+    print(number)
