5e4a56bca113e4e1d8661f53948d19022c73581d
Andrej <pytpoint@gmail.com>
2026-03-04 21:40:13 +0100

    o6

--- read.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# ДЕСЕРИАЛИЗАЦИЯ (Процесс распаковки из файла)
-# 1. Открываем тот же файл, но теперь для чтения ('r' - read)
-with open("data.json", "r") as f:
-    # 2. Команда load "загружает" данные обратно в переменную
-    yesterday_data = json.load(f)
-
-print(f"Программа вспомнила! Вчерашний пробег: {yesterday_data['mileage']} км. and last city war {yesterday_data['last_city']} ")
